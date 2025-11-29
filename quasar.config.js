7c9f9d6c15d8c4ec11b9f375c5892d56d819e1f2
Lin Htut Kyaw <linhtut123451@gmail.com>
2025-11-30 02:21:03 +0630

    generate linux dev

--- quasar.config.js
+++ quasar.config.js
@@ -197,7 +197,7 @@ export default defineConfig((ctx) => {
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
 
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: 'builder', // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -213,7 +213,14 @@ export default defineConfig((ctx) => {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'quasarsupabasechatgpt',
+        appId: 'com.dpsmap.linhtutkyaw',
+        productName: 'Dpsmap',
+        linux: {
+          target: ['deb'],
+          category: 'Utility',
+          maintainer: 'Lin Htut Kyaw <linhtutkyaw@example.com>',
+          vendor: 'Dpsmap',
+        },
       },
     },
 
